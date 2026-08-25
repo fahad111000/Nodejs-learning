78303b79e107606ca4d5df4fe29568f8154515dd
fahad111000 <fahad.kh2210@gmail.com>
2026-08-26 00:12:29 +0500

    HTTP info

---
+++ day-02 https/server.js
@@ -0,0 +1,52 @@
+console.log("Hello World! day 02");
+
+// HTTP:
+// The Rules between client and server are called HTTP(protocol)
+// (data is transfered between client and server using HTTP protocol)
+
+// Request:
+// The data which want by client is to the server is called request. The request is sent by the client to the server. The request contains the data which is required by the client.
+//  The request is sent by the client to the server using HTTP protocol.
+
+// Response:
+// The data which is sent by the server to the client is called response.
+//
+
+// HTTP Methods:
+// 1. GET:Tha data which we wanted from the server using GET method.
+// 2. POST: The data which we want to send to the server using POST method.
+// 3. PUT: The data which we want to update on the server using PUT method.
+// 4. DELETE: The data which we want to delete from the server using DELETE method.
+
+// GET = Revice, POST= send, PUT= update, DELETE= remove
+
+// URL:
+// This is the address  of the server which is used to send the request to the server.
+//  The URL contains the protocol, domain name, port number, and path of the server.
+
+//  Headers:
+// Extra information which is sent by the client to the server is called headers.
+// The headers contain the information about the request and response.
+// The headers are sent by the client to the server using HTTP protocol.
+// Example:
+//  Content-Type, Content-Length, Authorization, etc.
+// Content-Type: application/json
+
+// Body:
+// The actual data which we needed
+
+// HTTP Status Codes:
+// 1. 200: OK
+// 2. 201: Created
+// 3. 400: Bad Request
+// 4. 401: Unauthorized
+// 5. 403: Forbidden
+// 6. 404: Not Found
+// 7. 500: Internal Server Error
+
+// req:client requst info,  res: server response info
+
+// req.method: GET, POST, PUT, DELETE
+// req.url: /, /about, /contact
+// req.end(): end the request
+
